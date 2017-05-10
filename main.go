ccc62bf14fa94c212aa35136a98e11acd37315c6
qinains <qinains@qq.com>
2017-05-11 00:57:03 +0800

    init: add fsnotify package

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	fsnotify "gopkg.in/fsnotify.v1"
+)
+
+func main() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				log.Println("event:", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("modified file:", event.Name)
+				}
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	cwd, _ := os.Getwd()
+	err = watcher.Add(cwd)
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
+}
